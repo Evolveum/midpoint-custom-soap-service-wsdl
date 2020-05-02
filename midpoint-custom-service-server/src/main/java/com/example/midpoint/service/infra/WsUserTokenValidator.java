36a4f46bc57f2495063f3ff2f0ef80c32a5df497
Richard Richter <richard.richter@evolveum.com>
2020-05-02 18:16:15 +0200

    added WsUserTokenValidator, password validation without callback

---
+++ midpoint-custom-service-server/src/main/java/com/example/midpoint/service/infra/WsUserTokenValidator.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2010-2017 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+package com.example.midpoint.service.infra;
+
+import org.apache.wss4j.common.ext.WSSecurityException;
+import org.apache.wss4j.dom.handler.RequestData;
+import org.apache.wss4j.dom.message.token.UsernameToken;
+import org.apache.wss4j.dom.validate.Credential;
+import org.apache.wss4j.dom.validate.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+
+import com.evolveum.midpoint.model.api.AuthenticationEvaluator;
+import com.evolveum.midpoint.model.api.context.PasswordAuthenticationContext;
+import com.evolveum.midpoint.schema.constants.SchemaConstants;
+import com.evolveum.midpoint.security.api.ConnectionEnvironment;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
+
+/**
+ * Validator of user token - tries to authenticated with provided username and password.
+ */
+public class WsUserTokenValidator implements Validator {
+
+    // logs on info level for demonstration purposes, adjust accordingly
+    private static final Trace LOGGER = TraceManager.getTrace(WsUserTokenValidator.class);
+
+    @Autowired
+    private AuthenticationEvaluator<PasswordAuthenticationContext> passwordAuthenticationEvaluator;
+
+    @Override
+    public Credential validate(Credential credential, RequestData data) throws WSSecurityException {
+        // few lines taken from default UsernameTokenValidator, but we don't want it all
+        if (credential == null || credential.getUsernametoken() == null) {
+            throw new WSSecurityException(WSSecurityException.ErrorCode.FAILURE, "noCredential");
+        }
+
+        UsernameToken usernameToken = credential.getUsernametoken();
+        PasswordAuthenticationContext authCtx = new PasswordAuthenticationContext(
+                usernameToken.getName(), usernameToken.getPassword(), UserType.class);
+        LOGGER.info("WS validation: username='{}', password type='{}'",
+                usernameToken.getName(), usernameToken.getPasswordType());
+
+        ConnectionEnvironment connEnv =
+                ConnectionEnvironment.create(SchemaConstants.CHANNEL_WEB_SERVICE_URI);
+        try {
+            passwordAuthenticationEvaluator.checkCredentials(connEnv, authCtx);
+        } catch (AccessDeniedException | AuthenticationException e) {
+            LOGGER.error("Access/auth exception in validate - {}: {}",
+                    e.getClass().getSimpleName(), e.getMessage());
+            throw new WSSecurityException(WSSecurityException.ErrorCode.FAILURE, e.getMessage());
+        }
+
+        return credential;
+    }
+}
